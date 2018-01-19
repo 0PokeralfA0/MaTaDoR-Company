5acc9b5dcce2bd61bad4ac985cf976033e15eab9
0PokeralfA0 <33123300+0PokeralfA0@users.noreply.github.com>
2018-01-19 18:07:15 +0330

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,12 @@
+SUDO_ID = {256576176,443746244,499948268} --آیدی سودو های ربات (ربات cli , api  قرار بدید)
+Full_Sudo = {256576176} -- آیدی سودو اصلی
+TD_ID = 443746244 -- آیدی ربات
+Sudoid = 256576176 -- آیدی سودو اصلی
+BotHelper = 499948268 -- آیدی هلپر
+UserBotHelper = '@pokerhelppbOt' -- یوزرنیم هلپر
+LinkSuppoRt = 'https://t.me/joinchat/D0sKsFEwYu0h3QBJ6H9JdA' -- لینک گروه ساپورت
+UserSudo = '@poker_alfa' -- یوزرنیم سازنده
+PvUserSudo = '@pmpoker_bot' -- پیام رسان سازنده
+Channel = '@mini_hacker' -- یوزرنیم کانال 
+ChannelInline = 'mini_hacker' --یوزنیم کانال بدون @
+token = '499948268:AAGxi1PkFx74hD4-n4NiR3TRhoU1AuxGPzs' -- توکن ربات api
